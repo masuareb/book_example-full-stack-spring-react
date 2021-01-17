225c2eab9aed48909bd14fede086b31f29b57381
Arturo Suárez <masuareb@hotmail.com>
2021-01-17 17:19:03 -0600

    Configure CommandLineRunner to insert Car entity examples to H2 database

--- src/main/java/dev/masuareb/fullstackapp/FullstackAppApplication.java
+++ src/main/java/dev/masuareb/fullstackapp/FullstackAppApplication.java
@@ -1,13 +1,21 @@
 package dev.masuareb.fullstackapp;
 
+import dev.masuareb.fullstackapp.domain.Car;
+import dev.masuareb.fullstackapp.domain.CarRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class FullstackAppApplication {
 	private static final Logger logger = LoggerFactory.getLogger(FullstackAppApplication.class);
+
+	@Autowired
+	private CarRepository repository;
 
 	public static void main(String[] args) {
 		// Add comment for test Dev Tools
@@ -16,4 +24,13 @@ public class FullstackAppApplication {
 		logger.info("Hello Spring Boot");
 	}
 
+	@Bean
+	CommandLineRunner runner() {
+		return args -> {
+			// Save demo data to database
+			repository.save(new Car("Ford", "Mustang", "Red", "ADF-1121", 2017, 59000));
+			repository.save(new Car("Nissan", "Leaf", "White", "SSJ-3002", 2014, 29000));
+			repository.save(new Car("Toyota", "Prius", "Silver", "KKO-0212", 2018, 39000));
+		};
+	}
 }
