c835ba9576f25e1e31efdb9948633771e3576b9d
Arturo Suárez <masuareb@hotmail.com>
2021-01-17 16:05:54 -0600

    Add logging

--- src/main/java/dev/masuareb/fullstackapp/FullstackAppApplication.java
+++ src/main/java/dev/masuareb/fullstackapp/FullstackAppApplication.java
@@ -1,13 +1,19 @@
 package dev.masuareb.fullstackapp;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class FullstackAppApplication {
+	private static final Logger logger = LoggerFactory.getLogger(FullstackAppApplication.class);
 
 	public static void main(String[] args) {
+		// Add comment for test Dev Tools
+		// updated
 		SpringApplication.run(FullstackAppApplication.class, args);
+		logger.info("Hello Spring Boot");
 	}
 
 }
