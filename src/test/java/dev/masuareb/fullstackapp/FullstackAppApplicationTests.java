b68e8ed8a2e3dff11856b986d1408f99e04aa585
Arturo Suárez <masuareb@hotmail.com>
2021-01-17 15:50:01 -0600

    FullStack Example App - Iniital Commit

---
+++ src/test/java/dev/masuareb/fullstackapp/FullstackAppApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.masuareb.fullstackapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullstackAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
