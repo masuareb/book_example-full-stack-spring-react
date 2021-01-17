16bd4eabf94db720fb6a1fc4ea6d90bc67130b6c
Arturo Suárez <masuareb@hotmail.com>
2021-01-17 17:33:56 -0600

    JPA Query Examples

--- src/main/java/dev/masuareb/fullstackapp/domain/CarRepository.java
+++ src/main/java/dev/masuareb/fullstackapp/domain/CarRepository.java
@@ -2,5 +2,15 @@ package dev.masuareb.fullstackapp.domain;
 
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface CarRepository extends CrudRepository<Car, Long> {
+    // Fetch cars by brand
+    List<Car> findByBrand(String brand);
+
+    // Fetch cars by color
+    List<Car> findByColor(String color);
+
+    // Fetch cars by year
+    List<Car> findByYear(int year);
 }
