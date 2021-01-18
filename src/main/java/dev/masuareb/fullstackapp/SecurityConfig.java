a84f209c3bb1b1dfc03b321cc0a2e38a83e5153a
Arturo Suárez <masuareb@hotmail.com>
2021-01-17 21:57:02 -0600

    Add User entity, repository and service to enable authentication from users in database.

--- src/main/java/dev/masuareb/fullstackapp/SecurityConfig.java
+++ src/main/java/dev/masuareb/fullstackapp/SecurityConfig.java
@@ -1,13 +1,17 @@
 package dev.masuareb.fullstackapp;
 
+import dev.masuareb.fullstackapp.service.UserDetailServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 import javax.persistence.criteria.CriteriaBuilder;
@@ -15,21 +19,12 @@ import javax.persistence.criteria.CriteriaBuilder;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
+    @Autowired
+    private UserDetailServiceImpl userDetailsService;
 
-    }
-
-    @Bean
-    @Override
-    public UserDetailsService userDetailsService() {
-        UserDetails user =
-                User.withDefaultPasswordEncoder()
-                .username("user")
-                .password("password")
-                .roles("USER")
-                .build();
-
-        return new InMemoryUserDetailsManager(user);
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService)
+                .passwordEncoder(new BCryptPasswordEncoder());
     }
 }
