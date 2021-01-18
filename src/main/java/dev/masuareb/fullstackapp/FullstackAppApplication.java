f6f71f10f4611b949e5ca4850c9afff876bb9c11
Arturo Suárez <masuareb@hotmail.com>
2021-01-17 22:26:12 -0600

    Added example users to database

--- src/main/java/dev/masuareb/fullstackapp/FullstackAppApplication.java
+++ src/main/java/dev/masuareb/fullstackapp/FullstackAppApplication.java
@@ -1,9 +1,6 @@
 package dev.masuareb.fullstackapp;
 
-import dev.masuareb.fullstackapp.domain.Car;
-import dev.masuareb.fullstackapp.domain.CarRepository;
-import dev.masuareb.fullstackapp.domain.Owner;
-import dev.masuareb.fullstackapp.domain.OwnerRepository;
+import dev.masuareb.fullstackapp.domain.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,6 +18,10 @@ public class FullstackAppApplication {
 
 	@Autowired
 	private OwnerRepository orepository;
+
+	@Autowired
+	private UserRepository urepository;
+
 	public static void main(String[] args) {
 		// Add comment for test Dev Tools
 		// updated
@@ -41,6 +42,11 @@ public class FullstackAppApplication {
 			repository.save(new Car("Ford", "Mustang", "Red", "ADF-1121", 2017, 59000, owner1));
 			repository.save(new Car("Nissan", "Leaf", "White", "SSJ-3002", 2014, 29000, owner2));
 			repository.save(new Car("Toyota", "Prius", "Silver", "KKO-0212", 2018, 39000, owner2));
+
+			// username: user password: user
+			urepository.save(new User("user", "$2a$04$1.YhMIgNX/8TkCKGFUONWO1waedKhQ5KrnB30fl0Q01QKqmzLf.Zi", "USER"));
+			// username: admin password: admin
+			urepository.save(new User("admin", "$2a$04$KNLUwOWHVQZVpXyMBNc7JOzbLiBjb9Tk9bP7KNcPI12ICuvzXQQKG", "ADMIN"));
 		};
 	}
 }
