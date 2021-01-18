03882e1661277dfa6fe5e3dd62c94571c0d251a4
Arturo Suárez <masuareb@hotmail.com>
2021-01-17 20:53:43 -0600

    Configure REST Car repository

--- src/main/java/dev/masuareb/fullstackapp/domain/CarRepository.java
+++ src/main/java/dev/masuareb/fullstackapp/domain/CarRepository.java
@@ -1,15 +1,18 @@
 package dev.masuareb.fullstackapp.domain;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import java.util.List;
 
+@RepositoryRestResource
 public interface CarRepository extends CrudRepository<Car, Long> {
     // Fetch cars by brand
-    List<Car> findByBrand(String brand);
+    List<Car> findByBrand(@Param("brand") String brand);
 
     // Fetch cars by color
-    List<Car> findByColor(String color);
+    List<Car> findByColor(@Param("color") String color);
 
     // Fetch cars by year
     List<Car> findByYear(int year);
