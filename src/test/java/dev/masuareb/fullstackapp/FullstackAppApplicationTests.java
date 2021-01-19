fbdc8623c0cbc70f578d970813f9aa3ae2421175
Arturo Suárez <masuareb@hotmail.com>
2021-01-18 22:50:33 -0600

    Testing if controller is correctly injected

--- src/test/java/dev/masuareb/fullstackapp/FullstackAppApplicationTests.java
+++ src/test/java/dev/masuareb/fullstackapp/FullstackAppApplicationTests.java
@@ -1,13 +1,20 @@
 package dev.masuareb.fullstackapp;
 
+import dev.masuareb.fullstackapp.web.CarController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class FullstackAppApplicationTests {
+	@Autowired
+	private CarController controller;
 
 	@Test
 	void contextLoads() {
+		assertThat(controller).isNotNull();
 	}
 
 }
